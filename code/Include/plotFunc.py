929cfd06261d83b4307f0e52c53ec4f1992d6079
roiade-tensorflow <2024719067@qq.com>
2019-09-06 08:01:34 +0800

    '增加显示函数'

--- code/Include/plotFunc.py
+++ code/Include/plotFunc.py
@@ -8,3 +8,28 @@ def imageShow(image,scale=2):
     dir=np.where(data>0)
     plt.scatter(dir[0],dir[1],s=scale)
     plt.show()
+
+def imageShow2(imageNose ,imageEval,imageLable,scale=2):
+
+    dataNose=np.array(imageNose)
+    dataEval=np.array(imageEval)
+    dataLabel=np.array(imageLable)
+
+    plt.figure(figsize=(10,10))
+
+    plt.xlim(0,imageNose.shape[0])
+    plt.ylim(0,imageNose.shape[1])
+
+    dirNose=np.where(dataNose>0)
+    dirEval=np.where(dataEval>0)
+    dirLabel=np.where(dataLabel>0)
+
+    axNose=plt.axes([0,0,0.5,0.5])
+    axEval=plt.axes([0,0.5,0.5,0.5])
+    axLabel=plt.axes([0.5,0.5,0.5,0.5])
+
+    axNose.scatter(dirNose[0],dirNose[1],s=scale)
+    axEval.scatter(dirEval[0],dirEval[1],s=scale)
+    axLabel.scatter(dirLabel[0],dirLabel[1],s=scale)
+
+    plt.show()
--- nnModel/test.py
+++ nnModel/test.py
@@ -20,39 +20,42 @@ if __name__ == '__main__':
     lossAll=[]
     lossmin = 10000
     net.load_state_dict(torch.load('./weight/net_save_200nose.pth'))
-    im, an = next(a)
+    for k in range(6):
+        im, an = next(a)
 
-    im = im.float()
-    an = an.float()
+        im = im.float()
+        an = an.float()
 
-    im = im.permute([0, 4, 1, 2, 3]).float()
-    targets = an
-    if torch.cuda.is_available():
-        im = im.cuda()
-        net = net.cuda()
-        targets = an.cuda()
+        im = im.permute([0, 4, 1, 2, 3]).float()
+        targets = an
+        if torch.cuda.is_available():
+            im = im.cuda()
+            net = net.cuda()
+            targets = an.cuda()
 
-    im = Variable(im)
-    targets = Variable(targets)
+        im = Variable(im)
+        targets = Variable(targets)
 
-    yy=net(im)
+        yy=net(im)
 
-    yy=yy.cpu().detach().numpy()
+        yy=yy.cpu().detach().numpy()
 
-    dirTa = np.where(yy > 0.93)
-    targetsFromNet=np.zeros_like(yy)
-    targetsFromNet[dirTa[0],dirTa[1],dirTa[2],dirTa[3],dirTa[4]]=1
-    imagesForDectet = im.cpu().detach().numpy()
-    detectedResult=np.logical_and(imagesForDectet ,targetsFromNet )
-    print(targets.size())
+        dirTa = np.where(yy > 0.85)
+        targetsFromNet=np.zeros_like(yy)
+        targetsFromNet[dirTa[0],dirTa[1],dirTa[2],dirTa[3],dirTa[4]]=1
+        imagesForDectet = im.cpu().detach().numpy()
+        detectedResult=np.logical_and(imagesForDectet ,targetsFromNet )
+        print(targets.size())
 
-    targets=targets.cpu().detach().numpy()
-    imageShow(detectedResult[0,0,:,:,:])
-    imageShow(targets[0,  :, :, :])
-    imageShow(imagesForDectet[0,0, :, :, :])
+        targets=targets.cpu().detach().numpy()
 
-    # print(detectedResult.sum())
-    # print('len(dirTa)',len(dirTa))
-    # print(dirTa)
+        imageShow2(imagesForDectet[0,0, :, :, :],detectedResult[0,0,:,:,:],targets[0,  :, :, :])
+        # imageShow(detectedResult[0,0,:,:,:])
+        # imageShow(targets[0,  :, :, :])
+        # imageShow(imagesForDectet[0,0, :, :, :])
+
+        # print(detectedResult.sum())
+        # print('len(dirTa)',len(dirTa))
+        # print(dirTa)
 
 
